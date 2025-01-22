1e5519bf0f541e8812a87ad2932c16dc633f88f1
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-01-23 00:58:51 +0530

    Implemented Search Functionality in Search Bar

--- src/components/CountriesContainer.jsx
+++ src/components/CountriesContainer.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './Search'
 import DropdownBar from './DropdownBar'
 import CountryCard from './CountryCard'
 import CountryData from '../../countriesData';
 
 const CountriesContainer = () => {
+
   // CountryData.sort((countryA, countryB) => {
   //   return countryA.name.common.localeCompare(countryB.name.common);
   // });
+
+  const [query, setQuery] = useState('');
+
   return (
     <main className='countries-container'>
         <div className="filter-section">
-            <Search />
+            <Search setQuery={setQuery}/>
             <DropdownBar />
         </div>
         <div className="countries-list">
           {
-            CountryData.map((country) => {
+            CountryData.filter((country) => 
+              country.name.common.toLowerCase().includes(query)
+            ).map((country) => {
               return (
                 <CountryCard 
+                  key={country.name.common}
                   flagURL={country.flags.svg}
                   countryName={country.name.common}
                   population={country.population}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Search = () => {
+const Search =({setQuery}) => {
   return (
     <div className='search-section'>
         <i className="fa-solid fa-magnifying-glass"></i>
-        <input type="text" placeholder="Search for a country..." />
+        <input 
+          type="text" 
+          placeholder="Search for a country..." 
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}   
+        />
     </div>
   )
 }
